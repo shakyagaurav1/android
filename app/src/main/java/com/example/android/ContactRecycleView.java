a6971a3c2ea232bf8d3788ef502b47dba46beea0
dell <shakyagaurav1.3468@gmail.com>
2021-07-25 12:52:28 +0545

    Fifth_class

--- app/src/main/java/com/example/android/ContactRecycleView.java
+++ app/src/main/java/com/example/android/ContactRecycleView.java
@@ -1,12 +1,21 @@
 package com.example.android;
 
+
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
@@ -15,8 +24,10 @@ class ContactRecViewAdapter extends RecyclerView.Adapter<ContactRecViewAdapter.V
 
     private ArrayList<Contact> contacts = new ArrayList<>();
 
-    public ContactRecViewAdapter(){
+    public  Context context;
 
+    public ContactRecViewAdapter(Context context){
+        this.context = context;
     }
 
     @NonNull
@@ -32,7 +43,21 @@ class ContactRecViewAdapter extends RecyclerView.Adapter<ContactRecViewAdapter.V
     //View holder argument
     //position => index of the item in the array
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.contactName.setText(contacts.get(0).getName());
+        holder.contactName.setText(contacts.get(position).getName());
+        holder.contactEmail.setText((contacts.get(position).getEmail()));
+
+        // to set onClickListener to the card view itself
+        holder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(context, v, contacts.get(position).getName() + " Selected", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        // to render image URLs
+        Glide.with(context)
+                .asBitmap().load(contacts.get(position).getImageUrl())
+                .into(holder.imageView);
     }
 
     @Override
@@ -47,11 +72,17 @@ class ContactRecViewAdapter extends RecyclerView.Adapter<ContactRecViewAdapter.V
     }
 
     //inner class
+    //responsible for holding the view items for evey item
     public class ViewHolder extends RecyclerView.ViewHolder{
-        private TextView contactName;
+        private TextView contactName, contactEmail;
+        private CardView parent;
+        private ImageView imageView;
         public ViewHolder(@NonNull View itemView){
             super(itemView);
             contactName = itemView.findViewById(R.id.contactName);
+            contactEmail = itemView.findViewById(R.id.contactEmail);
+            parent = itemView.findViewById(R.id.contactListParent);
+            imageView = itemView.findViewById(R.id.recyclerViewImage);
         }
 
     }
--- app/src/main/java/com/example/android/RecyclerView.java
+++ app/src/main/java/com/example/android/RecyclerView.java
@@ -1,18 +1,36 @@
 package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class RecyclerView extends AppCompatActivity {
 
-    private View contactsRecView;
+    private androidx.recyclerview.widget.RecyclerView contactsRecView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
 
         contactsRecView = findViewById(R.id.recyclerView);
+
+        ArrayList<Contact> contacts = new ArrayList<>();
+        contacts.add(new Contact("Dwayne Johnson","dwayne@johnson.com","https://www.google.com.np/search?q=dwayne+johnson&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjlqeLAyv3xAhU3zjgGHT7HBF8Q_AUoAXoECAEQAw&biw=1366&bih=657#imgrc=iMYjUqFsmdKSKM.jpg"));
+        contacts.add(new Contact("Johnny Depp","johnny@depp.com","https://i.guim.co.uk/img/media/5827279817bdc9e8c7b655c8f6798c2d53ca1170/0_21_3000_1801/master/3000.jpg?width=1020&quality=85&auto=format&fit=max&s=5db1746fc50ef3dbf65d6ebc5f96665b"));
+        contacts.add(new Contact("Emma Stone","emma@stone.com","https://m.economictimes.com/thumb/msid-75534218,width-1200,height-900,resizemode-4,imgsize-524277/emma-stone-shared-a-mental-health-advice-for-people-during-the-pandemic-urging-them-to-spend-more-time-writing-instead-of-fretting-.jpg"));
+        contacts.add(new Contact("Jenifer lawrence","jenifer@lawrence.com","https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTQzMjgyNDgwNjIxODIzNTU5/jennifer-lawrence_gettyimages-626382596jpg.jpg"));
+
+        ContactRecViewAdapter contactRecViewAdapter = new ContactRecViewAdapter(this);
+
+        contactRecViewAdapter.setContacts(contacts);
+
+        contactsRecView.setAdapter(contactRecViewAdapter);
+
+        // programatically managed layout
+        contactsRecView.setLayoutManager(new GridLayoutManager(this, 2));
     }
 }
