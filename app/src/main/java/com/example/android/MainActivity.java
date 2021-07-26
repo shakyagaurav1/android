2fc45c0ef1fd1eeea7bb24a95780be8b490f7582
dell <shakyagaurav1.3468@gmail.com>
2021-07-26 12:35:37 +0545

    Second_assignment

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button frameLayout = findViewById(R.id.framelayoutbutton);
+
+        frameLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,FrameLayout.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
